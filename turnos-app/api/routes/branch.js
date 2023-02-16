2de1b82a8519b1c8560dc2b4f524af004f1b92b4
juan-ma526 <jmperez675@gmail.com>
2023-02-16 06:22:51 -0300

    fixx

--- turnos-app/api/routes/branch.js
+++ turnos-app/api/routes/branch.js
@@ -17,7 +17,7 @@ const {
 } = require("../controllers/branch");
 
 router.post("/createBranch", validateAuth, validateAdmin, createBranch);
-router.get("/allBranchs", allBranchs);
+router.get("/allBranchs", allBranchs); //router.get("/allBranchs", validateAuth, validateAdmin, allBranchs)
 router.get("/branchById/:id", validateAuth, validateAdmin, branchById);
 router.get("/operators/:id", validateAuth, validateAdmin, allOperatorsInBranch);
 router.put("/updateBranch/:id", validateAuth, validateAdmin, updateBranch);
